f3a4fde7651a99fdbf75a00c8439ec2aea4e5613
alisson <alissongouvei@gmail.com>
2019-02-17 21:47:35 -0400

    update

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, viewport-fit=cover">
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+    <link rel="sortcut icon" href="public/img/iavp.icon" type="image/x-icon" />
+    <title>IAVP | COLLEGE</title>
+
+    <!--DIRECTORY CSS-->
+
+    <!-- <link rel="stylesheet" href="public/css/bootstrap.css"> -->
+    <link rel="stylesheet" href="public/css/slides.min.css">
+
+
+</head>
+<body class="slides horizontal simplifiedMobile animated">
+
+    <?php include "public/containers/Menu.html";?>
+    <?php include "public/containers/icon.html";?>
+    <?php include "public/containers/Home.html";?>
+    <?php include "public/containers/Informacao.html";?>
+    <?php include "public/containers/Videos.html";?>
+    <?php include "public/containers/Rodape.html";?>
+
+     
+
+
+<!--DIRECTORY JAVASCRIPT-->
+<script src="public/js/jquery.min.js"></script>
+<script src="public/js/bootstrap.min.js"></script>
+<script src="public/js/plugins.js"></script>
+<script src="public/js/slides.js"></script>
+
+</body>
+</html>
